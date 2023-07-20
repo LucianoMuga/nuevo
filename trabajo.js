73f3d0bd7f5aaf96f0adaab5288d6d3e24b1e580
Luciano Muga <lucho.muga2@gmail.com>
2023-07-20 13:17:46 -0300

    mi primer commit

---
+++ trabajo.js
@@ -0,0 +1,32 @@
+let nombre = "Luciano";
+let apellido = "Muga";
+let socio = true;
+let numeroDeSocio = 27;
+let fechaDelPartido = "27/08";
+let horaDelPartido = "21:30";
+let precioDelBoleto = 17000;
+let arr = [1,2, "hola"]
+
+console.log(nombre)
+console.log(typeof(nombre))
+
+console.log(apellido)
+console.log(typeof(apellido))
+
+console.log(socio)
+console.log(typeof(socio))
+
+console.log(numeroDeSocio)
+console.log(typeof(numeroDeSocio))
+
+console.log(fechaDelPartido)
+console.log(typeof(fechaDelPartido))
+
+console.log(horaDelPartido)
+console.log(typeof(horaDelPartido))
+
+console.log(precioDelBoleto)
+console.log(typeof(precioDelBoleto))
+
+console.log(arr)
+console.log(typeof(arr))
